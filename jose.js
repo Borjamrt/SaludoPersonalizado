11ef7f442f2819dd7ad76797976319b58202e379
Borjamrt <borjamrt@outlook.com>
2024-02-21 11:10:49 +0100

    jose

---
+++ jose.js
@@ -0,0 +1,8 @@
+function sumar(){
+    const nombre = document.getElementById(Nombre);
+    const apellido = document.getElementById(apellido);
+    document.getElementById("Saluda").innerHTML = saludo;
+    const botonSaludo = document.getElementById("saluda");
+    botonSaluda.addEventListener("click, sumar");
+
+}
